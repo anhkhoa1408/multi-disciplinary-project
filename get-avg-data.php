d04c5fc285843a10eedb77f7a927d0313372240b
TK <thaikhuong62000@gmail.com>
2021-06-17 17:38:58 +0700

    Change  avg-chart behavior

--- get-avg-data.php
+++ get-avg-data.php
@@ -4,11 +4,12 @@
 
     $userName = $_SESSION['user'];
 
-    $query = "SELECT AVG(`Temperature`) AS `Average_Temperature`, AVG(`Humidity`) AS `Average_Humidity`, DATE(`Time_Receive`) `Time` FROM `parameter` WHERE DATE(`Time_Receive`) = DATE(NOW()) AND `userName` = '$userName' GROUP BY `Time`";
+    $query = "SELECT AVG(`Temperature`) AS `Average_Temperature`, AVG(`Humidity`) AS `Average_Humidity`, DATE(`Time_Receive`) `Time` FROM `parameter` WHERE `Time_Receive` = (SELECT MAX(`Time_Receive`) FROM `parameter` WHERE `userName` = '$userName') AND `userName` = '$userName' GROUP BY `Time_Receive`";
     $result = $conn->query($query) or die($conn->error);
     $data = array();
     foreach ($result as $row) {
         $data[] = $row;
     }
+
     echo json_encode($data);
 ?>
